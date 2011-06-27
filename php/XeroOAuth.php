d90eba91a2e714d655383747620241baa03efe5d
Ronan Quirke <ronan.quirke@gmail.com>
2011-06-27 21:47:27 +1200

    Added XeroAuth.php
    external file for Xero specific settings and classes

---
+++ php/XeroOAuth.php
@@ -0,0 +1,23 @@
+<?php
+
+// different app method defaults
+$xro_defaults = array( 'xero_url'     => 'https://api.xero.com/api.xro/2.0',
+                     'site'    => 'https://api.xero.com',
+                     'authorize_url'    => 'https://api.xero.com/oauth/Authorize',
+                     'signature_method'    => 'RSA-SHA1');
+                     
+$xro_partner_defaults = array( 'xero_url'     => 'https://api-partner.network.xero.com/api.xro/2.0',
+                     'site'    => 'https://api-partner.network.xero.com',
+                     'authorize_url'    => 'https://api.xero.com/oauth/Authorize',
+                     'signature_method'    => 'RSA-SHA1');
+                     
+$xro_partner_mac_defaults = array( 'xero_url'     => 'https://api-partner2.network.xero.com/api.xro/2.0',
+                     'site'    => 'https://api-partner2.network.xero.com',
+                     'authorize_url'    => 'https://api.xero.com/oauth/Authorize',
+                     'signature_method'    => 'RSA-SHA1');
+                     
+// standard Xero OAuth stuff
+$xro_consumer_options = array( 'request_token_path'    => '/oauth/RequestToken',
+                     'access_token_path'    => '/oauth/AccessToken',
+                     'authorize_path'    => '/oauth/Authorize');
+                     
